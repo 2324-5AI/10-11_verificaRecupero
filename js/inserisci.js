5850a5d1b0224beea32656d20555f45a67c4a8e1
Daniele Fea <daniele.fea@vallauri.edu>
2023-10-12 10:26:20 +0200

    Correzione richieste

--- js/inserisci.js
+++ js/inserisci.js
@@ -5,5 +5,5 @@
     PER QUESTA PAGINA
     5.1) In corrispondenza del bottone INSERISCI è necessario controllare i campi compilati
     5.2) Quando inserisco i dati devo controllare che non ci sia già lo slot di quel giorno 
-        pieno, in tal caso lo aggiorno sostiendo le info precedenti con quelle nuove
+        pieno, in tal caso lo aggiorno sostituendo le info precedenti con quelle nuove
  */
